80610621dc87f8fa9792bfa994530b4a1b5d1328
skeptycal <skeptycal@gmail.com>
2020-12-31 18:47:08 -0600

    add func DirNames

--- devtools/codeinfo/stats.go
+++ devtools/codeinfo/stats.go
@@ -7,8 +7,22 @@ import (
 	"github.com/skeptycal/util/gofile"
 )
 
-func Lines(ext string) int {
-
+// DirNames returns the contents of the directory path and returns
+// a slice containing names of files in the directory, in directory
+// order. Subsequent calls on the same file will yield further names.
+//
+// [...] If n <= 0, Readdirnames returns all the names from the
+// directory in a single slice. In this case, if Readdirnames succeeds (reads all the way to the end of the directory), it returns the slice and a nil error. If it encounters an error before the end of the directory, Readdirnames returns the names read until that point and a non-nil error.
+func DirNames(path, ext string) ([]string, error) {
+	if path == "" {
+		path = gofile.PWD()
+	}
+	f, err := os.Open(path)
+	if err != nil {
+		return []string{}, err
+	}
+	defer f.Close()
+	return f.Readdirnames(-1)
 }
 
 func IsEmpty(path string) (bool, error) {
