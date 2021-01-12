116dc391f9ed3dda3ff15b5cc1aa754415f830af
skeptycal <skeptycal@gmail.com>
2021-01-12 15:08:05 -0600

    GoBot [v0.3.0-1610485684+396546000]: dev (pre v1.0) autosave - progress and formatting  M  webtools/stack/cmd/stack/main.go M  webtools/stack/go.sum

--- webtools/stack/cmd/stack/main.go
+++ webtools/stack/cmd/stack/main.go
@@ -2,42 +2,72 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/kardianos/service"
+	log "github.com/sirupsen/logrus"
 )
 
 const serviceName = "Medium service"
 const serviceDescription = "Simple service, just for fun"
 
-var logger service.Logger
+var (
+	serviceIsRunning bool
+	programIsRunning bool
+	writingSync      sync.Mutex
+)
+
+// var logger service.Logger
 
 type program struct{}
 
 func (p program) Start(s service.Service) error {
 	// Start should not block. Do the actual work async.
 	fmt.Println(s.String() + " started")
+
+	writingSync.Lock()
+	serviceIsRunning = true
+	writingSync.Unlock()
+
 	go p.run()
 	return nil
 }
 
 func (p program) Stop(s service.Service) error {
 	// Stop should not block. Return with a few seconds.
+	writingSync.Lock()
+	serviceIsRunning = false
+	writingSync.Unlock()
+
+	for programIsRunning {
+		fmt.Println(s.String() + " stopping...")
+		time.Sleep(1 * time.Second)
+	}
+
 	fmt.Println(s.String() + " stopped")
 	return nil
 }
 
 func (p program) run() {
-	for {
+
+	for serviceIsRunning {
 		// Do work here
+
+		writingSync.Lock()
+		programIsRunning = true
+		writingSync.Unlock()
 		fmt.Println("Service is running")
 		time.Sleep(1 * time.Second)
+		writingSync.Lock()
+		programIsRunning = false
+		writingSync.Unlock()
 	}
 }
 
 func main() {
 
-	logger.Info("logger started...")
+	log.Info("logger started...")
 
 	serviceConfig := &service.Config{
 		Name:        serviceName,
@@ -47,10 +77,10 @@ func main() {
 	prg := &program{}
 	s, err := service.New(prg, serviceConfig)
 	if err != nil {
-		logger.Errorf("Cannot create the service: %v\n", err.Error())
+		log.Errorf("Cannot create the service: %v\n", err.Error())
 	}
 	err = s.Run()
 	if err != nil {
-		logger.Errorf("Cannot start the service: " + err.Error())
+		log.Errorf("Cannot start the service: " + err.Error())
 	}
 }
