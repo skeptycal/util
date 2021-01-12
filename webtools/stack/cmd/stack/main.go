4715e039a895b2b9c7a90df81ce7d21b526a413f
skeptycal <skeptycal@gmail.com>
2021-01-12 14:37:34 -0600

    GoBot [v0.3.0-1610483853+351943000]: dev (pre v1.0) autosave - progress and formatting  M  go.sum R  webtools/stack/static/homepage.js -> webtools/stack/cmd/stack/homepage.js R  webtools/stack/static/index.css -> webtools/stack/cmd/stack/index.css R  webtools/stack/static/index.html -> webtools/stack/cmd/stack/index.html M  webtools/stack/cmd/stack/main.go A  webtools/stack/go.sum M  webtools/stack/stack.go

---
+++ webtools/stack/cmd/stack/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kardianos/service"
+)
+
+const serviceName = "Medium service"
+const serviceDescription = "Simple service, just for fun"
+
+type program struct{}
+
+func (p program) Start(s service.Service) error {
+	panic("implement me")
+}
+
+func (p program) Stop(s service.Service) error {
+	panic("implement me")
+}
+
+func main() {
+	serviceConfig := &service.Config{
+		Name:        serviceName,
+		DisplayName: serviceName,
+		Description: serviceDescription,
+	}
+	prg := &program{}
+	s, err := service.New(prg, serviceConfig)
+	if err != nil {
+		fmt.Println("Cannot create the service: " + err.Error())
+	}
+	err = s.Run()
+	if err != nil {
+		fmt.Println("Cannot start the service: " + err.Error())
+	}
+}
---
+++ webtools/stack/stack.go
@@ -0,0 +1,3 @@
+// Package stack generates a very basic web stack with
+// Go (v1.15.6) and Vanilla Javascript (Node v15.4.0).
+package stack
