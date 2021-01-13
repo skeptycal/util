0b1bbdd1b2a5f6e6d63091adbd35e9856e7bb771
skeptycal <skeptycal@gmail.com>
2021-01-12 22:30:21 -0600

    GoBot [v0.3.0-1610512217+252426000]: dev (pre v1.0) autosave - progress and formatting  R  webtools/stack/cmd/stack/index.css -> webtools/stack/cmd/stack/css/index.css D  webtools/stack/cmd/stack/homepage.js A  webtools/stack/cmd/stack/html/homepage.html R  webtools/stack/cmd/stack/index.html -> webtools/stack/cmd/stack/html/index.html A  webtools/stack/cmd/stack/js/homepage.js M  webtools/stack/cmd/stack/main.go M  webtools/stack/go.sum M  webtools/stack/stack.go

--- webtools/stack/cmd/stack/main.go
+++ webtools/stack/cmd/stack/main.go
@@ -1,69 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"sync"
-	"time"
-
 	"github.com/kardianos/service"
 	log "github.com/sirupsen/logrus"
-)
-
-const serviceName = "Medium service"
-const serviceDescription = "Simple service, just for fun"
-
-var (
-	serviceIsRunning bool
-	programIsRunning bool
-	writingSync      sync.Mutex
+	"github.com/skeptycal/util/webtools/stack"
 )
 
 // var logger service.Logger
 
-type program struct{}
-
-func (p program) Start(s service.Service) error {
-	// Start should not block. Do the actual work async.
-	fmt.Println(s.String() + " started")
-
-	writingSync.Lock()
-	serviceIsRunning = true
-	writingSync.Unlock()
-
-	go p.run()
-	return nil
-}
-
-func (p program) Stop(s service.Service) error {
-	// Stop should not block. Return with a few seconds.
-	writingSync.Lock()
-	serviceIsRunning = false
-	writingSync.Unlock()
-
-	for programIsRunning {
-		fmt.Println(s.String() + " stopping...")
-		time.Sleep(1 * time.Second)
-	}
-
-	fmt.Println(s.String() + " stopped")
-	return nil
-}
-
-func (p program) run() {
-
-	for serviceIsRunning {
-		// Do work here
-
-		writingSync.Lock()
-		programIsRunning = true
-		writingSync.Unlock()
-		fmt.Println("Service is running")
-		time.Sleep(1 * time.Second)
-		writingSync.Lock()
-		programIsRunning = false
-		writingSync.Unlock()
-	}
-}
+const serviceName = "Medium service"
+const serviceDescription = "Simple service, just for fun"
 
 func main() {
 
@@ -74,8 +20,9 @@ func main() {
 		DisplayName: serviceName,
 		Description: serviceDescription,
 	}
-	prg := &program{}
-	s, err := service.New(prg, serviceConfig)
+
+	s, err := stack.NewService(serviceConfig)
+
 	if err != nil {
 		log.Errorf("Cannot create the service: %v\n", err.Error())
 	}
--- webtools/stack/stack.go
+++ webtools/stack/stack.go
@@ -1,3 +1,98 @@
 // Package stack generates a very basic web stack with
-// Go (v1.15.6) and Vanilla Javascript (Node v15.4.0).
+// Go and Javascript.
+//
+// An interface and configuration are provided and a service is
+// returned
 package stack
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/kardianos/service"
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	serviceIsRunning bool
+	programIsRunning bool
+	writingSync      sync.Mutex
+)
+
+func NewService(c *service.Config) (service.Service, error) {
+	i := NewInterface()
+	s, err := service.New(i, c)
+	if err != nil {
+		log.Errorf("Cannot create the service: %v\n", err.Error())
+	}
+	return s, err
+}
+
+func NewInterface() Program {
+	return &program{}
+}
+
+type Program interface {
+	Start(s service.Service) error
+	Stop(s service.Service) error
+	run()
+}
+
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	// Start should not block. Do the actual work async.
+
+	writingSync.Lock()
+	serviceIsRunning = true
+	writingSync.Unlock()
+
+	fmt.Println(s.String() + " started")
+
+	go p.run()
+	return nil
+}
+
+func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return within a few seconds.
+	writingSync.Lock()
+	serviceIsRunning = false
+	writingSync.Unlock()
+
+	// for programIsRunning {
+	// wait for cleanup ...
+	fmt.Println(s.String() + " stopping...")
+	time.Sleep(500 * time.Millisecond)
+	// }
+	fmt.Println(s.String() + " stopped")
+	return nil
+}
+
+func (p *program) run() {
+	// for serviceIsRunning {
+	// 	writingSync.Lock()
+	// 	programIsRunning = true
+	// 	writingSync.Unlock()
+
+	router := httprouter.New()
+	router.ServeFiles("/js/*filepath", http.Dir("js"))
+	router.ServeFiles("/css/*filepath", http.Dir("css"))
+
+	router.GET("/", serveHomepage)
+
+	err := http.ListenAndServe(":81", router)
+	if err != nil {
+		log.Errorf("Problem starting web server: %v", err.Error())
+	}
+	// 	writingSync.Lock()
+	// 	programIsRunning = false
+	// 	writingSync.Unlock()
+	// }
+}
+
+func serveHomepage(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+
+}
