000e8ca7c8823dca5d9768eeec59fde346a3a90b
skeptycal <skeptycal@gmail.com>
2021-01-16 17:57:44 -0600

    GoBot [v0.3.0-1610841461+127828000]: dev (pre v1.0) autosave - progress and formatting  M  _examples/sbrom/main.go

--- _examples/sbrom/main.go
+++ _examples/sbrom/main.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -50,6 +51,7 @@ type Ansi struct {
 	sb *strings.Builder
 }
 
+// Build encodes a variadic list of bytes into ANSI 7 bit escape codes.
 func (a *Ansi) Build(b ...byte) string {
 	defer a.sb.Reset()
 	for _, n := range b {
@@ -58,33 +60,122 @@ func (a *Ansi) Build(b ...byte) string {
 	return a.sb.String()
 }
 
-func (a *Ansi) Print(v ...interface{}) (int, error) {
-	fmt.Fprint(a.w, v...)
-	for _, s := range v {
-		if b, ok := s.(byte); ok {
-
-		}
-	}
-	return fmt.Print(a.Build(v...))
+// Set accepts, encodes, and prints a variadic argument list of bytes
+// that represent ANSI colors.
+func (a *Ansi) Set(v ...byte) (int, error) {
+	return fmt.Fprint(a.w, a.Build(v...))
 }
 
+// Unmarshal parses the JSON-encoded data and stores the result
+// in the value pointed to by v. If v is nil or not a pointer,
+// Unmarshal returns an InvalidUnmarshalError.
+//
+// Unmarshal uses the inverse of the encodings that
+// Marshal uses, allocating maps, slices, and pointers as necessary,
+// with the following additional rules:
+//
+// To unmarshal JSON into a pointer, Unmarshal first handles the case of
+// the JSON being the JSON literal null. In that case, Unmarshal sets
+// the pointer to nil. Otherwise, Unmarshal unmarshals the JSON into
+// the value pointed at by the pointer. If the pointer is nil, Unmarshal
+// allocates a new value for it to point to.
+//
+// To unmarshal JSON into a value implementing the Unmarshaler interface,
+// Unmarshal calls that value's UnmarshalJSON method, including
+// when the input is a JSON null.
+// Otherwise, if the value implements encoding.TextUnmarshaler
+// and the input is a JSON quoted string, Unmarshal calls that value's
+// UnmarshalText method with the unquoted form of the string.
+//
+// To unmarshal JSON into a struct, Unmarshal matches incoming object
+// keys to the keys used by Marshal (either the struct field name or its tag),
+// preferring an exact match but also accepting a case-insensitive match. By
+// default, object keys which don't have a corresponding struct field are
+// ignored (see Decoder.DisallowUnknownFields for an alternative).
+//
+// To unmarshal JSON into an interface value,
+// Unmarshal stores one of these in the interface value:
+//
+//	bool, for JSON booleans
+//	float64, for JSON numbers
+//	string, for JSON strings
+//	[]interface{}, for JSON arrays
+//	map[string]interface{}, for JSON objects
+//	nil for JSON null
+//
+// To unmarshal a JSON array into a slice, Unmarshal resets the slice length
+// to zero and then appends each element to the slice.
+// As a special case, to unmarshal an empty JSON array into a slice,
+// Unmarshal replaces the slice with a new empty slice.
+//
+// To unmarshal a JSON array into a Go array, Unmarshal decodes
+// JSON array elements into corresponding Go array elements.
+// If the Go array is smaller than the JSON array,
+// the additional JSON array elements are discarded.
+// If the JSON array is smaller than the Go array,
+// the additional Go array elements are set to zero values.
+//
+// To unmarshal a JSON object into a map, Unmarshal first establishes a map to
+// use. If the map is nil, Unmarshal allocates a new map. Otherwise Unmarshal
+// reuses the existing map, keeping existing entries. Unmarshal then stores
+// key-value pairs from the JSON object into the map. The map's key type must
+// either be any string type, an integer, implement json.Unmarshaler, or
+// implement encoding.TextUnmarshaler.
+//
+// If a JSON value is not appropriate for a given target type,
+// or if a JSON number overflows the target type, Unmarshal
+// skips that field and completes the unmarshaling as best it can.
+// If no more serious errors are encountered, Unmarshal returns
+// an UnmarshalTypeError describing the earliest such error. In any
+// case, it's not guaranteed that all the remaining fields following
+// the problematic one will be unmarshaled into the target object.
+//
+// The JSON null value unmarshals into an interface, map, pointer, or slice
+// by setting that Go value to nil. Because null is often used in JSON to mean
+// ``not present,'' unmarshaling a JSON null into any other Go type has no effect
+// on the value and produces no error.
+//
+// When unmarshaling quoted strings, invalid UTF-8 or
+// invalid UTF-16 surrogate pairs are not treated as an error.
+// Instead, they are replaced by the Unicode replacement
+// character U+FFFD.
+//
 func (a *Ansi) Unmarshal(data []byte, v interface{}) error {
 	return nil
+	json.Unmarshal()
+}
+
+// Unmarshaler is the interface implemented by types
+// that can unmarshal a JSON description of themselves.
+// The input can be assumed to be a valid encoding of
+// a JSON value. UnmarshalJSON must copy the JSON data
+// if it wishes to retain the data after returning.
+//
+// By convention, to approximate the behavior of Unmarshal itself,
+// Unmarshalers implement UnmarshalJSON([]byte("null")) as a no-op.
+type Unmarshaler interface {
+	UnmarshalJSON([]byte) error
 }
 
 func (a *Ansi) Marshal(v interface{}) ([]byte, error) {
 	return nil, nil
 }
 
+// String returns the contents of the underlying strings.Builder and
+// resets the buffer to nil in preparation for the next call.
 func (a *Ansi) String() string {
 	defer a.sb.Reset()
 	return a.sb.String()
 }
 
+// Write implements io.Writer and writes the byte slice to the underlying
+// strings.Builder
 func (a *Ansi) Write(b []byte) (int, error) {
 	return a.w.Write(b)
 }
 
+// WriteString implements io.StringWriter and writes the string contents
+// to the underlying strings.Builder
 func (a *Ansi) WriteString(s string) (int, error) {
 	return a.w.WriteString(s)
 }
