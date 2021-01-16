5aabe560899400cb7c05d606da87bc334c2ff802
skeptycal <skeptycal@gmail.com>
2021-01-16 15:23:42 -0600

    GoBot [v0.3.0-1610832200+735931000]: dev (pre v1.0) autosave - progress and formatting  A  datatools/upspin/upspin.go

---
+++ datatools/upspin/upspin.go
@@ -0,0 +1,10 @@
+// Package upspin is an online, encrypted, sharable, personal information service.
+// "I would never encourage anyone to break the law. On the flipside,
+// I don't want the law to break my data."
+//
+// upspin implements an interface that keeps the law from breaking my data.
+//
+// https://upspin.io/
+//
+// https://github.com/upspin/upspin
+package upspin
