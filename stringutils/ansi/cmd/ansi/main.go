eef5733a0ef0513ae22f980268d6e064bddb0ed5
skeptycal <skeptycal@gmail.com>
2021-01-20 14:47:25 -0600

    GoBot [v0.3.0-1611175620+941319000]: dev (pre v1.0) autosave - progress and formatting  M  stringutils/ansi/cmd/ansi/main.go

--- stringutils/ansi/cmd/ansi/main.go
+++ stringutils/ansi/cmd/ansi/main.go
@@ -1,9 +1,25 @@
 package main
 
-import "github.com/skeptycal/util/stringutils/ansi"
+import (
+	"fmt"
+
+	"github.com/skeptycal/util/stringutils/ansi"
+)
 
 func main() {
     w := ansi.NewANSIWriter(nil)
-    w.WriteString("WriteString tests")
-    w.Write("Write Text")
+    w.WriteString("WriteString tests\n")
+    w.Write([]byte("Write Text\n"))
+
+   s :=  w.String()
+   fmt.Println(s)
+
+   fmt.Println("Flush ... ")
+   w.Flush()
+
+   w.Wrap("Wrap test\n")
+
+   w.Build(ansi.Italics, ansi.Red,ansi.YellowBackground)
+
+   fmt.Println("Build test (should be: italic red text on yellow background")
 }
