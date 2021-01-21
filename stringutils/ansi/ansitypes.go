3cef498d3b638843dbd29a72f30484dd3011d762
skeptycal <skeptycal@gmail.com>
2021-01-20 21:25:22 -0600

    GoBot [v0.3.0-1611199519+290051000]: dev (pre v1.0) autosave - progress and formatting  M  stringutils/ansi/ansitypes.go

--- stringutils/ansi/ansitypes.go
+++ stringutils/ansi/ansitypes.go
@@ -68,21 +68,21 @@ func (a *ansi24) String() string { return a.out }
 type ansiSetType struct {
     depth AnsiStyle
     format string
-	fg    string
-	bg    string
-	ef    string
+	fg    byte
+	bg    byte
+	ef    byte
 	out   string
 }
 
-func (a ansiSetType) BG() string     { return a.bg }
-func (a ansiSetType) FG() string     { return a.fg }
+func (a ansiSetType) BG() string     { return fmt.Sprintf(a.format,background,a.bg) }
+func (a ansiSetType) FG() string     { return fmt.Sprintf(a.format,foreground,a.fg) }
 func (a ansiSetType) Info() string { return fmt.Sprint(a.fg) }
 func (a ansiSetType) Output() string { return fmt.Sprintf("%v;%v;%v", a.ef, a.fg, a.bg) }
 
 var styleFormat = map[AnsiStyle]string{
-	StyleNormal: "\x1b[%v%vm",
+	StyleNormal: "\x1b%vm",
     StyleAnsi8bit:   "\x1b[%v8;5;%vm", // [38;5;${ID}m
-	StyleAnsi24bit:    "\x1b[%%v8;2;%vm",
+	StyleAnsi24bit:    "\x1b[%v8;2;%vm",
 }
 
 // SetColors creates printable strings for each of the ansi effects
@@ -104,16 +104,16 @@ func (a ansiSetType) SetColors(fg, bg, ef color) {
     */
     a.format = styleFormat[a.depth]
 
-    a.fg = fmt.Sprintf(a.format, foreground, fg&BasicMask)
+    a.fg = fg&BasicMask
+	a.bg = bg&BasicMask
+    a.ef = ef
+
+	o := fmt.Sprintf("%v;3%v;4%v", ef, fg, bg)
+
+    a.out = fmt.Sprintf(a.format, o)
 
     fmt.Printf("fg, a.fg: %q, %q\n",fg, a.fg)
 
-	a.bg = fmt.Sprintf(a.format, background ,bg&BasicMask)
-    a.ef = fmt.Sprintf(FMTansi, ef)
-
-	o := fmt.Sprintf("%v;3%v;4%v", ef, fg&BasicMask, bg&BasicMask)
-
-    a.out = fmt.Sprintf(a.format, o)
 }
 
 
