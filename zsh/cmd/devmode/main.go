33220780e640227407a5493cfc3ad60e513900e0
skeptycal <skeptycal@gmail.com>
2021-01-21 12:58:44 -0600

    GoBot [v0.3.0-1611255520+472083000]: dev (pre v1.0) autosave - progress and formatting  M  zsh/cmd/devmode/main.go

--- zsh/cmd/devmode/main.go
+++ zsh/cmd/devmode/main.go
@@ -1,7 +1,37 @@
 package main
 
-const ( configFile = "~/.dotfiles/)
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path"
+)
+
+const (
+
+    configFile = `.dotfiles/zshrc_inc/dev_mode.zsh`
+)
+
+func myfile(path string) (string, error) {
+
+}
+
+func getfile(path string) string {
+    cmd := exec.Command("echo", path )
+    b, err := cmd.Output()
+    if err != nil {
+        log.Fatal(err)
+        return ""
+    }
+    return string(b)
+}
 
 func main() {
+    home, _ := os.UserHomeDir()
+    filename := path.Join(home, configFile)
 
+
+
+
+    println(s)
 }
