3f76471e00c5ec8cd34bad81751cdb8e41a8d33c
skeptycal <skeptycal@gmail.com>
2021-01-23 15:55:47 -0600

    GoBot [v0.3.0-1611438923+547466000]: dev (pre v1.0) autosave - progress and formatting  M  stringutils/ascii/ascii_test.go

--- stringutils/ascii/ascii_test.go
+++ stringutils/ascii/ascii_test.go
@@ -76,15 +76,15 @@ func TestHexed(t *testing.T) {
 		{"1000000000", args{1000000000}, "3b9aca00"},
 		{"32767", args{32767}, "7fff"},
 		{"1e12", args{1e12}, "0x1.d1a94a2p+39"},
-        {"-0x1.23abcp+20", args{-0x1.23abcp+20}, "-0x1.23abcp+20"},
-        {"nil", args{nil}, "0x0"},
-        {"empty slice", args{[]byte{}}, "NaN"},
-        {"slice", args{[]byte{50}}, "NaN"},
-        {"map", args{make(map[int]int)}, "NaN"},
-        {"map", args{make(map[int]int,4)}, "NaN"},
-        {"true", args{true}, "0x1"},
-        {"false", args{false}, "0x0"},
-        {"string", args{"string"}, "NaN"},
+		{"-0x1.23abcp+20", args{-0x1.23abcp+20}, "-0x1.23abcp+20"},
+		{"nil", args{nil}, "0x0"},
+		{"empty slice", args{[]byte{}}, "NaN"},
+		{"slice", args{[]byte{50}}, "NaN"},
+		{"map", args{make(map[int]int)}, "NaN"},
+		{"map", args{make(map[int]int, 4)}, "NaN"},
+		{"true", args{true}, "0x1"},
+		{"false", args{false}, "0x0"},
+		{"string", args{"string"}, "NaN"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -104,7 +104,7 @@ func TestToOctal(t *testing.T) {
 		args args
 		want string
 	}{
-        // TODO: Add test cases.
+		// TODO: Add test cases.
 		{"1.234", args{1.234}, "float"},
 		{"2.43e22", args{2.43e22}, "float"},
 		{"0.00000008", args{0.00000008}, "float"},
@@ -116,17 +116,56 @@ func TestToOctal(t *testing.T) {
 		{"1000000000", args{1000000000}, "0o7346545000"},
 		{"32767", args{32767}, "0o77777"},
 		{"1e12", args{1e12}, "float"},
-        {"-0x1.23abcp+20", args{-0x1.23abcp+20}, "float"},
-        {"nil", args{nil}, "0o0"},
-        {"true", args{true}, "0o1"},
-        {"false", args{false}, "0o0"},
-        {"string", args{"string"}, "NaN"},
-
+		{"-0x1.23abcp+20", args{-0x1.23abcp+20}, "float"},
+		{"nil", args{nil}, "0o0"},
+		{"true", args{true}, "0o1"},
+		{"false", args{false}, "0o0"},
+		{"string", args{"string"}, "NaN"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ToOctal(tt.args.v); got != tt.want {
 				t.Errorf("ToOctal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestToBinary(t *testing.T) {
+	type args struct {
+		v interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+        // TODO: Add test cases.
+		{"1.234", args{1.234}, "0x1.3be76c8b43958p+00"},
+		{"2.43e22", args{2.43e22}, "0x1.49538f9945421p+74"},
+		{"0.00000008", args{0.00000008}, "0x1.5798ee2308c3ap-24"},
+		{"2", args{2}, "10"},
+		{"17", args{17}, "10001"},
+		{"100", args{100}, "1100100"},
+		{"255", args{255}, "11111111"},
+		{"256", args{256}, "100000000"},
+		{"1000000000", args{1000000000}, "111011100110101100101000000000"},
+		{"32767", args{32767}, "111111111111111"},
+		{"1e12", args{1e12}, "8192000000000000p-13"},
+		{"-0x1.23abcp+20", args{-0x1.23abcp+20}, "5131128709054464p-32"},
+		{"nil", args{nil}, "0x0"},
+		{"empty slice", args{[]byte{}}, "NaN"},
+		{"slice", args{[]byte{50}}, "NaN"},
+		{"map", args{make(map[int]int)}, "NaN"},
+		{"map", args{make(map[int]int, 4)}, "NaN"},
+		{"true", args{true}, "0x1"},
+		{"false", args{false}, "0x0"},
+		{"string", args{"string"}, "NaN"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ToBinary(tt.args.v); got != tt.want {
+				t.Errorf("ToBinary() = %v, want %v", got, tt.want)
 			}
 		})
 	}
