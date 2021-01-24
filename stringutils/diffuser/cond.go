aff54c7c3924c83edda808fbf10025140b82318b
skeptycal <skeptycal@gmail.com>
2021-01-23 23:10:54 -0600

    GoBot [v0.3.1-1611465028+540261000]: dev (pre v1.0) autosave - progress and formatting  M  stringutils/diffuser/cond.go C  stringutils/diffuser/cond.go -> stringutils/diffuser/cond_test.go M  stringutils/diffuser/tryout.go

--- stringutils/diffuser/cond.go
+++ stringutils/diffuser/cond.go
@@ -14,9 +14,14 @@ package diffuser
 
 import "sync"
 
-var mu sync.Mutex
+var (
+    mu sync.Mutex                // for loopBad
+    cond sync.Cond               // for loopGood
 
-var mutex = &sync.Mutex{} // from medium article
+    mutex = &sync.Mutex{} // from medium article
+
+)
+
 
 func mediumUpdate(item *string) {
     mutex.Lock()
@@ -27,11 +32,39 @@ func mediumUpdate(item *string) {
     mutex.Unlock()
 }
 
-func loop(items []string) {
+// loopBad is an example of a loop that uses a mutex with
+// constant locks and unlocks.
+//
+// The article discusses the broadcasting functionality of sync.Cond,
+//but overlooks its primary usecase: synchronizing based on a condition
+// (it's right in the name!).
+//
+// The mediumUpdate function above is from the article.
+//
+// You can think of sync.Cond as solving this problem: "I want to take action as soon as some condition is satisfied, but I can't just write a spin-loop, because those are inefficient. What I really want is to check the condition whenever it may have changed, and otherwise wait."
+//
+// That's why sync.Cond is almost always called in a loop. Imagine we have some goroutines that are processing a slice of items, and in another goroutine, we want to take some action when len(items) == 0. We could write a spin-loop:
+func loopBad(items []*string) {
     for done := false; !done; {
         mu.Lock()
         // process 'items' ...
         done = len(items) == 0
         mu.Unlock()
     }
+}
+
+// loopGood is an example of a loop that uses cond.
+//
+// But this is obviously terrible; we'll be constantly locking
+// and unlocking. Instead, we should use a sync.Cond
+//
+// Then, any time a goroutine modifies items, it should call
+// cond.Signal(). This way, we can ensure that this loop only
+// runs when the condition might be satisfied.
+func loopGood(items []*string) {
+    cond.L.Lock()
+    for len(items) != 0 {
+        cond.Wait()
+    }
+    cond.L.Unlock()
 }
---
+++ stringutils/diffuser/cond_test.go
@@ -0,0 +1,39 @@
+/*
+
+Reference: https://teivah.medium.com/a-closer-look-at-go-sync-package-9f4e4a28c35a
+
+The article discusses the broadcasting functionality of sync.Cond, but overlooks its primary usecase: synchronizing based on a condition (it's right in the name!).
+
+You can think of sync.Cond as solving this problem: "I want to take action as soon as some condition is satisfied, but I can't just write a spin-loop, because those are inefficient. What I really want is to check the condition whenever it may have changed, and otherwise wait."
+
+That's why sync.Cond is almost always called in a loop. Imagine we have some goroutines that are processing a slice of items, and in another goroutine, we want to take some action when len(items) == 0. We could write a spin-loop:
+
+*/
+
+package diffuser
+
+import "testing"
+
+func BenchmarkLoopBad(b *testing.B) {
+
+    for i := 0; i < b.N; i++ {
+        loopBad(items)
+    }
+}
+
+func Test_loopBad(t *testing.T) {
+	type args struct {
+		items []*string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			loopBad(tt.args.items)
+		})
+	}
+}
