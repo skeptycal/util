228f48008549dca9bae4f71267899cabe282d103
skeptycal <skeptycal@gmail.com>
2021-01-27 10:48:20 -0600

    GoBot [v0.3.1-1611766098+779521000]: dev (pre v1.0) autosave - progress and formatting  M  devtools/gogithub/_example/escape-seq/main.go

--- devtools/gogithub/_example/escape-seq/main.go
+++ devtools/gogithub/_example/escape-seq/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// stdOut := bufio.NewWriter(colorable.NewColorableStdout())
 	stdOut := anansi.Output
-	defer stdOut.Flush()
+	defer stdOut.Flush() // Write buffered writes before exiting
 
 	fmt.Fprint(stdOut, "\x1B[3GMove to 3rd Column\n")
 	fmt.Fprint(stdOut, "\x1B[1;2HMove to 2nd Column on 1st Line\n")
